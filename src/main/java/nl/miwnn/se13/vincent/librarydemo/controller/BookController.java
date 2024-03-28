b20d73b526eb8a2cd7383d287974f851e74a52a7
Vincent Velthuizen <vincent@luvistudio.nl>
2024-03-28 13:36:06 +0100

    Added book form

--- src/main/java/nl/miwnn/se13/vincent/librarydemo/controller/BookController.java
+++ src/main/java/nl/miwnn/se13/vincent/librarydemo/controller/BookController.java
@@ -4,7 +4,10 @@ import nl.miwnn.se13.vincent.librarydemo.model.Book;
 import nl.miwnn.se13.vincent.librarydemo.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,4 +32,21 @@ public class BookController {
 
         return "bookOverview";
     }
+
+    @GetMapping("/book/new")
+    private String showBookForm(Model model) {
+        model.addAttribute("book", new Book());
+
+        return "bookForm";
+    }
+
+    @PostMapping("/book/new")
+    private String saveBook(@ModelAttribute("book") Book bookToBeSaved, BindingResult result) {
+        if (!result.hasErrors()) {
+            bookRepository.save(bookToBeSaved);
+        }
+
+        return "redirect:/";
+    }
+
 }
